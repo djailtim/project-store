413d1d1093ba050bb0b9da60042dff4f990484fa
Natalia <nanademoura@gmail.com>
2023-03-25 01:33:49 -0300

    implementado o retorno de email no response

--- src/main/java/com/example/projectstore/api/responses/UserResponse.java
+++ src/main/java/com/example/projectstore/api/responses/UserResponse.java
@@ -7,6 +7,7 @@ import lombok.*;
 @Getter @Setter @ToString
 public class UserResponse {
     private String name;
+    private String email;
     private String country;
     private String currency;
 }
