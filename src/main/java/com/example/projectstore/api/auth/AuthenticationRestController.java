1bb7f8c5810084e48fea3d95e9273b64bbc7e718
Ailton Ferreira Luz <djailtim@msn.com>
2023-03-29 21:49:28 -0300

    refatorando service de authentication e tratamento de erro de usuario encontrado

--- src/main/java/com/example/projectstore/api/auth/AuthenticationRestController.java
+++ src/main/java/com/example/projectstore/api/auth/AuthenticationRestController.java
@@ -2,6 +2,7 @@ package com.example.projectstore.api.auth;
 
 import com.example.projectstore.api.model.User;
 import com.example.projectstore.api.repositories.UserRepository;
+import com.example.projectstore.api.services.AuthenticationService;
 import com.example.projectstore.api.system.JwtService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
@@ -21,15 +22,12 @@ public class AuthenticationRestController {
     private final UserRepository userJpaRepository;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
     public AuthenticationResponse login(@RequestBody AuthenticationRequest request){
-        var authentication = new UsernamePasswordAuthenticationToken(request.username(), request.password());
-        authenticationManager.authenticate(authentication);
-        User user = userJpaRepository.findByEmail(request.username()).orElseThrow();
-        String token = jwtService.createToken(user);
-        return  new AuthenticationResponse(user.getId(), token);
+        return authenticationService.login(request.username(), request.password());
     }
 
 }
---
+++ src/main/java/com/example/projectstore/api/services/AuthenticationService.java
@@ -0,0 +1,35 @@
+package com.example.projectstore.api.services;
+
+import com.example.projectstore.api.auth.AuthenticationRequest;
+import com.example.projectstore.api.auth.AuthenticationResponse;
+import com.example.projectstore.api.exceptions.UserNotFoundException;
+import com.example.projectstore.api.model.User;
+import com.example.projectstore.api.repositories.UserRepository;
+import com.example.projectstore.api.system.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public AuthenticationResponse login(String email, String password) {
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isEmpty()) {
+            throw new UserNotFoundException("Usuário não encontrado.");
+        }
+        var authentication = new UsernamePasswordAuthenticationToken(email, password);
+        authenticationManager.authenticate(authentication);
+
+        String token = jwtService.createToken(user.get());
+        return new AuthenticationResponse(user.get().getId(), token);
+    }
+
+}
