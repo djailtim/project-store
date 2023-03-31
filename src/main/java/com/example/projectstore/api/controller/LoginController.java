ccae9fc6c936a132a90a8d6ad9e5000179b4707a
Natalia <nanademoura@gmail.com>
2023-03-31 00:52:53 -0300

    implementado html

---
+++ src/main/java/com/example/projectstore/api/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.example.projectstore.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+        @GetMapping("/storelogin")
+    public String boot(){
+        return "login-page";
+    }
+
+
+}
